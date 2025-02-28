44ba3f3862b9ab05fe9b5015d471b5d2afdb8550
LiaCabrall <lianav.cabral@gmail.com>
2025-02-28 14:30:19 -0300

    atualizações de habilidades

--- my-app/src/components/Habilidades.js
+++ my-app/src/components/Habilidades.js
@@ -4,33 +4,63 @@ export function Habilidades() {
     return (
         <section className={styles.text}>
             <h1>Habilidades</h1>
-            <div className={styles.lista}>
-                <ul>
-                    <li>
-                        Design de Interface
-                    </li>
-                    <li>
-                        Design de experiência do usuário
-                    </li>
-                    <li>
-                        Prototipagem
-                    </li>
-                    <li>
-                        Criação de Wireframes
-                    </li>
-                    <li>
-                        Pesquisa com Usuários
-                    </li>
-                    <li>
-                        Testes de Usabilidade
-                    </li>
-                    <li>
+            <div className={styles.listas}>
+                <div className={styles.lista}>
+                    <h2>Hard Skills</h2>
+                    <ul>
+                        <li>
+                            Design de Interface
+                        </li>
+                        <li>
+                            Design de experiência do usuário
+                        </li>
+                        <li>
                         Design Responsivo
-                    </li>
-                </ul>
+                        </li>
+                        <li>
+                            Criação de Wireframes
+                        </li>
+                        <li>
+                            Pesquisa com Usuários
+                        </li>
+                        <li>
+                            Testes de Usabilidade
+                        </li>
+                        <li>
+                            Prototipagem
+                        </li>
+                    </ul>
 
+                </div>
+
+                <div className={styles.lista}>
+                    <h2>Soft Skills</h2>
+                    <ul>
+                        <li>
+                            Resiliência
+                        </li>
+                        <li>
+                           Agilidade
+                        </li>
+                        <li>
+                            Pensamento crítico
+                        </li>
+                        <li>
+                            Flexibilidade
+                        </li>
+                        <li>
+                            Criatividade
+                        </li>
+                        <li>
+                            Tomada de decisões
+                        </li>
+                        <li>
+                            Comunicação e escuta ativa
+                        </li>
+                    </ul>
+
+                </div>
             </div>
-
         </section>
 
     )
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,12 +1,8 @@
 import styles from './Header.module.css';
-import foto1 from '../assets/foto1.png';
 
 export function Header() {
     return (
         <section className={styles.header}>
-            <div className={styles.img}>
-                <img src={foto1} alt='foto principal' />
-            </div>
             <div className={styles.text}>
                 <h1>
                     Liana Cabral
